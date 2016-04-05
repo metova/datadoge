fda3e8254c4eabb8204cc3fd46509c8f92d86f06
Jeff Cooper <jeff@cuperman.net>
2016-04-05 11:18:21 -0700

    Only send the custom metric name to DataDog

--- lib/datadoge.rb
+++ lib/datadoge.rb
@@ -23,11 +23,10 @@ module Datadoge
         host = "host:#{ENV['INSTRUMENTATION_HOSTNAME']}"
         status = event.payload[:status]
         tags = [controller, action, format, host]
-        metric = Datadoge.configuration.metric
-        ActiveSupport::Notifications.instrument metric, :action => :timing, :tags => tags, :measurement => "request.total_duration", :value => event.duration
-        ActiveSupport::Notifications.instrument metric, :action => :timing, :tags => tags, :measurement => "database.query.time", :value => event.payload[:db_runtime]
-        ActiveSupport::Notifications.instrument metric, :action => :timing, :tags => tags, :measurement => "web.view.time", :value => event.payload[:view_runtime]
-        ActiveSupport::Notifications.instrument metric, :tags => tags,  :measurement => "request.status.#{status}"
+        ActiveSupport::Notifications.instrument :performance, :action => :timing, :tags => tags, :measurement => "request.total_duration", :value => event.duration
+        ActiveSupport::Notifications.instrument :performance, :action => :timing, :tags => tags, :measurement => "database.query.time", :value => event.payload[:db_runtime]
+        ActiveSupport::Notifications.instrument :performance, :action => :timing, :tags => tags, :measurement => "web.view.time", :value => event.payload[:view_runtime]
+        ActiveSupport::Notifications.instrument :performance, :tags => tags,  :measurement => "request.status.#{status}"
       end
 
       ActiveSupport::Notifications.subscribe /performance/ do |name, start, finish, id, payload|
@@ -39,7 +38,7 @@ module Datadoge
         measurement = payload[:measurement]
         value = payload[:value]
         tags = payload[:tags]
-        key_name = "#{name.to_s}.#{measurement}"
+        key_name = "#{Datadoge.configuration.metric.to_s}.#{measurement}"
         if action == :increment
           $statsd.increment key_name, :tags => tags
         else
